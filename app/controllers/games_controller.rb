3e27c4ccd5b49268e29b39e90336d592aed24ed7
Eleni Marli <elenimarli@yahoo.gr>
2024-01-22 16:50:06 +0100

    Adding score using the session for 'Longest Word Game' challenge

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -17,17 +17,30 @@ class GamesController < ApplicationController
   end
 
   def result(answer, letters)
-    return "Sorry, but #{answer} can't be built out of #{letters.chars.join(', ')}" if in_grid?(answer, letters) == false
+    if in_grid?(answer, letters) == false
+      return ["Sorry, but #{answer} can't be built out of #{letters.chars.join(', ')}", 0]
+    end
 
     obj = JSON.parse(URI.open("https://wagon-dictionary.herokuapp.com/#{answer}").read)
-    return "Congratulations! #{answer} is a valid English word!" if obj['found'] == true
+    return ["Congratulations! #{answer} is a valid English word!", answer.length] if obj['found'] == true
 
-    "Sorry, but #{answer} does not seem to be a valid English word..."
+    ["Sorry, but #{answer} does not seem to be a valid English word...", 0]
+  end
+
+  def calculate_points(answer, letters, num)
+    if session[:points].nil?
+      session[:points] = num
+    else
+      session[:points] += num
+    end
   end
 
   def score
     answer = params[:answer].upcase
     letters = params[:letters]
-    @result = result(answer, letters)
+    result = result(answer, letters)
+    @result = result[0]
+    calculate_points(answer, letters, result[1])
+    @points = session[:points]
   end
 end
